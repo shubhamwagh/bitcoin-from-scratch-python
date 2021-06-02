899322df34a77f06866de2138b333098140bf969
Shubhan Wagh <shubham.wagh@q-bot.co>
2021-06-02 18:35:13 +0100

    Implemented all operations for finite field

--- src/playground.py
+++ src/playground.py
@@ -1,8 +1,13 @@
 from ecc import FieldElement
 
 if __name__ == "__main__":
-    f7 = FieldElement(3, 7)
-    f11 = FieldElement(7, 11)
+    f7 = FieldElement(3, 13)
+    f11 = FieldElement(7, 13)
     print(f7)
     print(f11)
-    print(f7 == f11)
+    print(f7 + f11)
+    print(f7 - f11)
+    print(f7 * f11)
+    print(f7 / f11)
+    print(f7 ** -3)
+    print(f7 != None)
