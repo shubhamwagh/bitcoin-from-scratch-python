2517633ed32ffcb7ae0f02ed7af571c31597e8a3
Shubhan Wagh <shubham.wagh@q-bot.co>
2021-06-02 22:27:57 +0100

    Added elliptic curve point addition

--- pybitcoin/ecc.py
+++ pybitcoin/ecc.py
@@ -54,3 +54,64 @@ class FieldElement:
 
         num = (self.num * pow(other.num, self.prime - 2, self.prime)) % self.prime
         return self.__class__(num, self.prime)
+
+
+class Point:
+    def __init__(self, x, y, a, b):
+        self.x = x
+        self.y = y
+        self.a = a
+        self.b = b
+        if self.x is None and self.y is None:
+            return
+        if self.y ** 2 != self.x ** 3 + a * self.x + b:
+            raise ValueError('({}, {}) is not on the curve'.format(x, y))
+
+    def __repr__(self):
+        if self.x is None:
+            return 'Point(infinity)'
+        else:
+            return 'Point({},{})_{}_{}'.format(self.x, self.y, self.a, self.b)
+
+    def __eq__(self, other):
+        return self.x == other.x and \
+               self.y == other.y and \
+               self.a == other.a and \
+               self.b == other.b
+
+    def __ne__(self, other):
+        return not (self == other)
+
+    def __add__(self, other):
+        if self.a != other.a or self.b != other.b:
+            raise TypeError('Points {}, {} are not on the same curve'.format
+                            (self, other))
+        if self.x is None:
+            # Handling infinity point case
+            return other
+
+        if other.x is None:
+            # Handling infinity point case
+            return self
+
+        if self.x == other.x and self.y != other.y:
+            # Handling vertical line case
+            return self.__class__(None, None, self.a, self.b)
+
+        if self.x != other.x:
+            # Handling general slant line case with valid slope intersecting at a point
+            slope = (other.y - self.y) / (other.x - self.x)
+            result_x = pow(slope, 2) - self.x - other.x
+            result_y = slope * (self.x - result_x) - self.y
+            return self.__class__(result_x, result_y, self.a, self.b)
+
+        if self == other and self.y == 0 * self.x:
+            # Handling vertical tangent line at y=0
+            return self.__class__(None, None, self.a, self.b)
+
+        if self == other:
+            # Handling general tangent line to elliptic curve intersecting at one point
+            slope = (3 * pow(self.x, 2) + self.a) / (2 * self.y)
+            x = pow(slope, 2) - 2 * self.x
+            y = slope * (self.x - x) - self.y
+            return self.__class__(x, y, self.a, self.b)
--- pybitcoin/playground.py
+++ pybitcoin/playground.py
@@ -1,4 +1,4 @@
-from ecc import FieldElement
+from ecc import FieldElement, Point
 
 if __name__ == "__main__":
     f7 = FieldElement(3, 13)
@@ -11,3 +11,8 @@ if __name__ == "__main__":
     print(f7 / f11)
     print(f7 ** -3)
     print(f7 != None)
+
+    p1 = Point(-1, -1, 5, 7)
+    p2 = Point(18, 77, 5, 7)
+    inf = Point(None, None, 5, 7)
+    print(p1 + p2)
