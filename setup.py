d30324f94f9b92e026f36d123d01932d5a264b59
Shubhan Wagh <shubham.wagh@q-bot.co>
2021-06-02 18:56:28 +0100

    Refactor

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='bitcoin-from-scratch-python',
+    version='0.1dev',
+    packages=['pybitcoin'],
+    license='MIT license',
+    long_description=open('README.md').read(),
+)
